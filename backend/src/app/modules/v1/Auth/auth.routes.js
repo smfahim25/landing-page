3cb124a09c3c45a2119168817230f77ff22575fc
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2024-08-25 03:34:42 +0600

    users get & change role system

--- backend/src/app/modules/v1/Auth/auth.routes.js
+++ backend/src/app/modules/v1/Auth/auth.routes.js
@@ -4,5 +4,7 @@ import { AuthController } from './auth.controller.js';
 const router = Router();
 
 router.post('/signup', AuthController.SignUp);
+router.get('/', AuthController.GetAllUsers);
+router.patch('/change-role', AuthController.ChangeRole);
 
 export const AuthRoutes = router;
