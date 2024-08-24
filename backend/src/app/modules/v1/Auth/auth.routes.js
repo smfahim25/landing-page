c092906137a21e42055e063af54e5a0ad744f231
Pranto Chakraborty <chakrabortypranto39@gmail.com>
2024-08-25 03:38:58 +0600

    Update auth.routes.js

--- backend/src/app/modules/v1/Auth/auth.routes.js
+++ backend/src/app/modules/v1/Auth/auth.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
+import auth from '../../../middlewares/auth.js';
 import { AuthController } from './auth.controller.js';
 
 const router = Router();
 
 router.post('/signup', AuthController.SignUp);
-router.get('/', AuthController.GetAllUsers);
-router.patch('/change-role', AuthController.ChangeRole);
+router.get('/', auth('ADMIN'), AuthController.GetAllUsers);
+router.patch('/change-role', auth('ADMIN'), AuthController.ChangeRole);
 
 export const AuthRoutes = router;
